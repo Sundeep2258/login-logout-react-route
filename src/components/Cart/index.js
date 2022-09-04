c1ecd79b03c1d8186bc1c6698d80fafbc0e281a8
sandep.B <sundeep@gmail.com>
2022-09-04 07:21:49 +0000

    update

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Products = () => (
+  <div className="bg">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+      alt="cart"
+      className="cart"
+    />
+  </div>
+)
+
+export default Products
---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Products = () => (
+  <div className="bg">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="Products"
+      className="product"
+    />
+  </div>
+)
+
+export default Products
